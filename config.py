4d73951fa2426043314aa3a7414c4f2166db707c
berkay <87997618+berkayda@users.noreply.github.com>
2023-11-23 21:02:29 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+telegram_chat_id_CHANNEL = str(-123456789) #WRITEYOURS
+telegram_chat_id_ME = str(123456789) #WRITEYOURS
+telegram_bot_token = 'WRITEYOURS'
+ticker = "BTC-USD"
+period = '1d'
+start = "2023-01-01"
+end = "2023-12-31"
